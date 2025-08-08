a1f5ab44601b454cf36ec034e80f73268e3892b5
maupessoagarcia <maupessoagarcia@yahoo.com>
2025-08-07 23:01:58 -0400

    test

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ from worker import speech_to_text, text_to_speech, openai_process_message
 from flask_cors import CORS
 import os
 
+#VAI TOMAR NO CU - TEST
+
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
